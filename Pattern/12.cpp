6b10914f5203430b6c34da4397100267ee15bc62
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-02-10 14:14:17 +0530

    Create 12.cpp
    Input: 5
    
    Output:
    1                 1
    1 2             2 1
    1 2 3         3 2 1
    1 2 3 4     4 3 2 1
    1 2 3 4 5 5 4 3 2 1

---
+++ Pattern/12.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i = 1; i <= n; i++){
+            for(int j =1; j <= i; j++)
+                cout<<j<<" ";
+            for(int k = 2*n - 2*i; k > 0; k--)
+                cout<<"  ";
+            for(int j =i; j > 0; j--)
+                cout<<j<<" ";
+            cout<<endl;
+        }
+    }
+};
+
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
