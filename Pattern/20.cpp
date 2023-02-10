f81c88d5a0564c7eb785ec09cd5e57a53aaee44e
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-02-10 21:54:13 +0530

    Create 20.cpp
    Input: 5
    
    Output:
    *        *
    **      **
    ***    ***
    ****  ****
    **********
    ****  ****
    ***    ***
    **      **
    *        *

---
+++ Pattern/20.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i = 1; i<=n; i++){
+            for(int j = i; j>0; j--)
+                cout<<"*";
+            for(int k = 2*(n-i); k>0; k--)
+                cout<<" ";
+            for(int j =i; j>0; j--)
+                cout<<"*";
+            cout<<"\n";
+        }
+        
+        for(int i = n-1; i>0; i--){
+            for(int j = i; j>0; j--)
+                cout<<"*";
+            for(int k = 2*(n-i); k>0; k--)
+                cout<<" ";
+            for(int j =i; j>0; j--)
+                cout<<"*";
+            cout<<"\n";
+        }
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
