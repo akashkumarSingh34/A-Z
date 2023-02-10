c77173d6ed050bcb713fe68390e43eab7dfb2d2a
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-02-10 22:24:41 +0530

    Create 10.cpp
    Input: 5
    
    Output:
    * 
    * * 
    * * * 
    * * * * 
    * * * * *
    * * * *
    * * *
    * *
    *

---
+++ Pattern/10.cpp
@@ -0,0 +1,13 @@
+    void printTriangle(int n) {
+        // code here
+        for(int i = 1; i <= n; i++){
+            for(int j = i; j > 0; j--)
+                cout<<"* ";
+            cout<<endl;
+        }
+        for(int i = n-1; i > 0; i--){
+            for(int j = i; j > 0; j--)
+                cout<<"* ";
+            cout<<endl;
+        }
+    }
