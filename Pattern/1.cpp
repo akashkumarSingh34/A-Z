85c29011601f9de6c5676a29857275f552c41dde
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-02-10 22:13:02 +0530

    Create 1.cpp
    Input: 5
    
    Output:
    * * * * *
    * * * * *
    * * * * *
    * * * * *
    * * * * *

---
+++ Pattern/1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
