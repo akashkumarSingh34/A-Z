413132def29c8cd3ae80ed8a2e856e78559aa627
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-02-10 22:14:33 +0530

    Create 2.cpp
    Input: 5
    
    Output:
    * 
    * * 
    * * * 
    * * * * 
    * * * * *

---
+++ Pattern/2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+      for(int i =1; i<=n; i++){
+            for(int j=0; j<i; j++)
+                cout<<"* ";
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
