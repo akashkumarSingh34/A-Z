9b8d33f339453942bf5ca5f8ef235033e4fb5a92
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-02-10 22:26:15 +0530

    Create 16.cpp
    Input: 5
    
    Output:
    A
    BB
    CCC
    DDDD
    EEEEE

---
+++ Pattern/16.cpp
@@ -0,0 +1,8 @@
+    void printTriangle(int n) {
+        // code here
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++)
+                cout<<char(65+i);
+            cout<<endl;
+        }
+    }
