3b35b22b14a1816f0bc218c5f30adc45b9cbd313
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-02-10 15:54:45 +0530

    Create 15.cpp
    Input: 5
    
    Output:
    ABCDE
    ABCD
    ABC
    AB
    A

---
+++ Pattern/15.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i =n; i>0; i--){
+            for(char j='A'; j<=64+i; j++)
+                cout<<j;
+            cout<<endl;
+        }
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
