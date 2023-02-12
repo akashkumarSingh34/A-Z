303f8a5ca54bbe07f412fefc898c9a559db09a8d
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-02-12 22:01:08 +0530

    Create Armstrong Number.cpp
    Input: N = 153
    Output: "Yes"
    Explanation: 153 is an Armstrong number
    since 13 + 53 + 33 = 153.
    Hence answer is "Yes".
    
    Input: N = 370
    Output: "Yes"
    Explanation: 370 is an Armstrong number
    since 33 + 73 + 03 = 370.
    Hence answer is "Yes".

---
+++ 1/Know Basic Maths/Armstrong Number.cpp
@@ -0,0 +1,10 @@
+    string armstrongNumber(int n){
+        // code here
+        int num = 0;
+        for(int i = n, rem = i%10; i > 0 ; i/=10, rem = i%10){
+            num += rem*rem*rem ;
+        }
+        if(num == n)
+            return "Yes";
+        return "No";
+    }
