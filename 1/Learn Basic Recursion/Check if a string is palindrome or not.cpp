64d067ba6bba8fd6edfcb5721b5913e0203324a6
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-02-13 22:05:25 +0530

    Create Check if a string is palindrome or not.cpp
    Input: S = "abba"
    Output: 1
    Explanation: S is a palindrome
    
    Input: S = "abc" 
    Output: 0
    Explanation: S is not a palindrome

---
+++ 1/Learn Basic Recursion/Check if a string is palindrome or not.cpp
@@ -0,0 +1,9 @@
+int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    for(int i = 0, j = S.size()-1; i<j; i++, j--){
+	        if(S[i] != S[j])
+	            return 0;
+	    }
+	   return 1;
+	}
