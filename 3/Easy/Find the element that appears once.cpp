f1faf1687eaf4335029ab15b347cddb74ba35423
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-03-02 01:29:24 +0530

    Create Find the element that appears once.cpp

---
+++ 3/Easy/Find the element that appears once.cpp
@@ -0,0 +1,23 @@
+// for sorted array	
+
+int search(int A[], int N){
+	    //code
+	   for(int i=0; i<=N-1; i+=2){
+	       if(A[i]!=A[i+1])
+	            return A[i];
+	   }
+	   return 0;
+	    
+	}
+
+
+// for an unsorted array
+
+	int search(int A[], int N){
+	    //code
+	    int number = 0;
+	   for(int i = 0; i<N; i++)
+	        number = number ^ A[i];
+	   return number;
+	    
+	}
