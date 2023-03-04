0a1e91cfbf28a3bdf11f66f1694262b5ebc92ee3
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2023-03-04 22:30:19 +0530

    Create if-else.cpp
    'lesser' if n < m
    'equal' if n == m
    'greater' if n > m

---
+++ 1/Things to Know in C++/if-else.cpp
@@ -0,0 +1,9 @@
+    string compareNM(int n, int m){
+        // code here
+        if(n<m)
+            cout<<"lesser";
+        else if(n==m)
+            cout<<"equal";
+        else
+            cout<<"greater";
+    }
